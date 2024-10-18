70cc00fc579cd4aaa75c0ee6adf08a98e720381e
AnastasiaDeka <99515576+AnastasiaDeka@users.noreply.github.com>
2024-10-18 23:47:38 +0300

    Исправлены ошибки

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
+
 from notes.models import Note
 from notes.forms import WARNING
 from pytils.translit import slugify
@@ -23,18 +24,30 @@ class TestNoteCreation(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.notes_counts = Note.objects.count()
         cls.user = User.objects.create(username='Пользователь')
         cls.auth_client = Client()
         cls.auth_client.force_login(cls.user)
-        cls.form_data = {
-            'text': cls.NOTE_TEXT,
-            'title': cls.NOTE_TITLE,
-            'slug': cls.NOTE_SLUG,
-            'author': cls.auth_client
+
+    def setUp(self):
+        self.form_data = {
+            'text': self.NOTE_TEXT,
+            'title': self.NOTE_TITLE,
+            'slug': self.NOTE_SLUG,
         }
 
+        self.note = Note.objects.create(
+            author=self.user,
+            title=self.NOTE_TITLE,
+            text=self.NOTE_TEXT,
+            slug=self.NOTE_SLUG
+        )
+
+        self.notes_counts = Note.objects.count()
+
     def test_user_can_create_note(self):
+        """Проверка, что авторизованный
+        пользователь может создать заметку.
+        """
         self.form_data['slug'] = 'new-unique-slug'
         response = self.auth_client.post(self.URL_TO_ADD, data=self.form_data)
         self.assertRedirects(response, self.URL_TO_DONE)
@@ -48,8 +61,8 @@ class TestNoteCreation(TestCase):
         self.assertEqual(note.author, self.user)
 
     def test_anonymous_user_cannot_create_note(self):
-        """Проверка, что анонимный
-        пользователь не может создать заметку.
+        """Проверка, что анонимный пользователь
+        не может создать заметку.
         """
         self.client.logout()
         response = self.client.post(self.URL_TO_ADD, data=self.form_data)
@@ -59,55 +72,24 @@ class TestNoteCreation(TestCase):
         self.assertEqual(notes_count, self.notes_counts)
 
     def test_slug_is_generated_automatically_if_not_provided(self):
-        """Проверка, что slug генерируется
-        автоматически, если не предоставлен.
+        """Проверка, что slug генерируется автоматически,
+        если не был указан пользователем.
         """
         self.form_data.pop('slug')
         response = self.auth_client.post(self.URL_TO_ADD, data=self.form_data)
         self.assertRedirects(response, self.URL_TO_DONE)
         self.assertEqual(Note.objects.count(),
                          self.notes_counts + self.EXPECTED_NOTES_INCREMENT)
-        new_note = Note.objects.get()
+        new_note = Note.objects.last()
         expected_slug = slugify(self.form_data['title'])
         self.assertEqual(new_note.slug, expected_slug)
 
-
-class TestNoteSlugDuplication(TestCase):
-    NOTE_TEXT = 'Текст заметки'
-    NOTE_TITLE = 'Текст заголовка'
-    NOTE_SLUG = 'unique-slug'
-
-    URL_TO_ADD = reverse('notes:add')
-    WARNING = "Должен быть уникальным."
-
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = User.objects.create(username='Пользователь')
-        cls.auth_client = Client()
-        cls.auth_client.force_login(cls.user)
-
-        cls.note = Note.objects.create(
-            author=cls.user,
-            title=cls.NOTE_TITLE,
-            text=cls.NOTE_TEXT,
-            slug=cls.NOTE_SLUG
-        )
-
-    def setUp(self):
-        self.form_data = {
-            'text': self.NOTE_TEXT,
-            'title': self.NOTE_TITLE,
-            'slug': self.NOTE_SLUG,
-        }
-        self.notes_counts = Note.objects.count()
-
     def test_user_cannot_create_note_with_duplicate_slug(self):
-        """Проверка, что пользователь
-        не может создать заметку с дублирующимся slug.
+        """Проверка, что пользователь не
+        может создать заметку с дублирующимся slug.
         """
         self.form_data['slug'] = self.note.slug
-        response = self.auth_client.post(self.URL_TO_ADD,
-                                         data=self.form_data)
+        response = self.auth_client.post(self.URL_TO_ADD, data=self.form_data)
 
         self.assertFormError(response, 'form', 'slug',
                              errors=(self.note.slug + WARNING))
